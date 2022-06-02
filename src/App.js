c22d4eba9f20f79d58c80f25b153edd857d6569d
devYeonjun <alcls0026@gmail.com>
2022-06-02 22:49:14 +1200

    Add axios get images from pixabay

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
 import Search from "./components/search/Search";
 
-function App() {
-	return (
-		<MuiThemeProvider>
-			<div>
-				<NavBar />
-				<Search />
-			</div>
-		</MuiThemeProvider>
-	);
+class App extends Component {
+	render() {
+		return (
+			<MuiThemeProvider>
+				<div>
+					<NavBar />
+					<Search />
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
-
 export default App;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,16 +2,30 @@ import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
 
 class Search extends Component {
 	state = {
 		searchText: "",
 		amount: 15,
-		apiUrl: "https://pixabay.com/api",
+		apiUrl: "https://pixabay.com/api/",
 		apiKey: "27723975-2450949546c16e213083ab408",
 		images: [],
 	};
+
+	onTextChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value }, () => {
+			axios
+				.get(
+					`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+				)
+				.then((res) => this.setState({ images: res.data.hits }))
+				.catch((err) => console.log(err));
+		});
+	};
+
 	render() {
+		console.log(this.state);
 		return (
 			<div>
 				<TextField
@@ -22,18 +36,18 @@ class Search extends Component {
 					fullWidth={true}
 				/>
 				<br />
-					<SelectField
-						name="amount"
-						floatingLabelText="Amount"
-						value={this.state.amount}
-						onChange={this.onAmountChange}
-					>
-						<MenuItem value={5} primaryText="5" />
-						<MenuItem value={10} primaryText="10" />
-						<MenuItem value={15} primaryText="15" />
-						<MenuItem value={30} primaryText="30" />
-						<MenuItem value={50} primaryText="50" />
-					</SelectField>
+				<SelectField
+					name="amount"
+					floatingLabelText="Amount"
+					value={this.state.amount}
+					onChange={this.onAmountChange}
+				>
+					<MenuItem value={5} primaryText="5" />
+					<MenuItem value={10} primaryText="10" />
+					<MenuItem value={15} primaryText="15" />
+					<MenuItem value={30} primaryText="30" />
+					<MenuItem value={50} primaryText="50" />
+				</SelectField>
 				<br />
 			</div>
 		);
