0ce8d3174deb4a2cbb628e3551156c14e4ba070f
devYeonjun <alcls0026@gmail.com>
2022-06-02 22:32:06 +1200

    Add selectfeild

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
 
 class Search extends Component {
 	state = {
@@ -20,6 +21,20 @@ class Search extends Component {
 					floatingLabelText="Search For Images"
 					fullWidth={true}
 				/>
+				<br />
+					<SelectField
+						name="amount"
+						floatingLabelText="Amount"
+						value={this.state.amount}
+						onChange={this.onAmountChange}
+					>
+						<MenuItem value={5} primaryText="5" />
+						<MenuItem value={10} primaryText="10" />
+						<MenuItem value={15} primaryText="15" />
+						<MenuItem value={30} primaryText="30" />
+						<MenuItem value={50} primaryText="50" />
+					</SelectField>
+				<br />
 			</div>
 		);
 	}
